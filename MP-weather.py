807e8c28654859182be2f95b028d53447633d96f
JasperKalf <jasp3r.kalf@gmail.com>
2016-10-31 20:45:37 +0100

    Progress Weather appli

---
+++ MP-weather.py
@@ -0,0 +1,16 @@
+
+owm = pyowm.OWM('1060b54135410b3cf1bd80a73b8ac92a')  # You MUST provide a valid API key
+
+# You have a pro subscription? Use:
+# owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
+
+# Will it be sunny tomorrow at this time in Milan (Italy) ?
+forecast = owm.daily_forecast("Milan,it")
+tomorrow = pyowm.timeutils.tomorrow()
+forecast.will_be_sunny_at(tomorrow)  # Always True in Italy, right? ;-)
+
+# Search for current weather in London (UK)
+observation = owm.weather_at_place('London,uk')
+w = observation.get_weather()
+print(w)                      # <Weather - reference time=2013-12-18 09:20,
+                              # status=Clouds>
