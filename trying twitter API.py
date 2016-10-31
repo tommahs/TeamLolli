ff0a50f44fdf48758aba199190feab526de915bc
tommahs <tommahs@tommahs.nl>
2016-10-31 19:23:55 +0100

    Working Iteration showing #, date/time of creation & text of the tweet

--- trying twitter API.py
+++ trying twitter API.py
@@ -11,13 +11,17 @@ consumer_secret = 'vJqX4rfOzI0xUviCzTZvacVmAjRGINdSFQlrmFuHydWdAHj29w'
 access_token_key = '751746308-MhnwFjcA1xkdgf0FTsDmbfiF0Bjv89ZEzhqrnDH6'
 access_token_secret = 'OXm1d9oAqSuO4FEjqZR4kwOy4gJITJEWGxCsM5K6vIw1o'
 
-oauth = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret )
+oauth = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
 
 # Requesting tweets from specific user: !! https://twitter.com/MarijnBecking
-r = oauth.request('statuses/user_timeline', {'count':2,'screen_name':'MarijnBecking'})
+r = oauth.request('statuses/user_timeline', {'count': 2,'screen_name' : 'MarijnBecking'})
+
 for item in r.get_iterator():
     if 'text' in item:
-        print (item) #On top of the term item can be placed an term from within the dict by doing a: item['*specified element']
+        print(item) #On top of the term item can be placed an term from within the dict by doing a: item['*specified element']
+
+
+
 # From this we get an "dict line with every element"(json)
 
 ## Trying to tweet something, dont use this one yet for this is later used
@@ -27,6 +31,34 @@ for item in r.get_iterator():
 # # Getting 50 recent tweets
 # r = oauth.request('statuses/home_timeline', {'count':50})
 # for item in r.get_iterator():
-#     if 'text' in item:
-#         print (item['text'])
+#     if item['text'] == 'tweet prog':
+#         print(item)
 
+     # if 'text' in item:
+     #     print(item['text'])
+
+
+#########
+# Tom Test
+# Reading tweets, sorting them based on the Hashtags
+r = oauth.request('statuses/user_timeline', {'count': 50}) #define wanted data from twitter
+tweets = []
+htlst=['ZombieHype', 'ShinyThing'] # using a list for the hashtags
+
+
+for item in r.get_iterator(): # Grab data from twitter
+    entities = item['entities'] # Accessing the subdictionary
+    hashtags = entities['hashtags'] # Is apparently a list of dictionaries
+    if 'text' in item: #if there is text in an item :
+        try: # Try/except to get rid of invalid data
+            for eachhashtag in hashtags: # for each hashtaglist in hashtags
+                for shortstation in htlst: # Check each possible abbreviation of the station
+                    if eachhashtag['text'] == shortstation: #Check if the hashtag equals a shortstation
+                        print('hashtag: #{} \ntext : {}'.format(eachhashtag['text'], item['text'])) # print the result
+                        print(item['created_at'])
+        except: # If the try fails with an error, do the following:
+            if IndexError: #if there isnt an hashtag in hashtags, it means that the list is empty, and gives an IndexError
+                continue # Continue the for-loop
+
+# str day month year, text
+# counter for checking the
