74dc654b8ed362205531cb933e3463e9f4c14b4e
Marijn <marijnbecking@gmail.com>
2016-10-31 23:32:57 +0100

    Twitter API tests

--- mainscreen.py
+++ mainscreen.py
@@ -1,6 +1,9 @@
 from authenticator import oauth
 
 r = oauth.request('statuses/user_timeline', {'count': 2,'screen_name' : 'MarijnBecking'})
-for item in r.get_iterator():
-    if 'text' in item:
-        print(item['text'])
+def requestadmin(status):
+    for item in r.get_iterator():
+        if 'text' in item:
+            message = item['text']
+            date_created_message = item['created_at']
+
