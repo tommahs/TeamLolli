a453117d49bf280d7f990d6f7e42bdd6190da149
tommahs <tommahs@tommahs.nl>
2016-11-01 01:29:55 +0100

    General userinput

--- mainclient.py
+++ mainclient.py
@@ -4,43 +4,52 @@
 ###
 
 # Twitter max characters = 140
+def userinput():
+    global fname, lname, text, date
+    from general_functions import inputquestion
+    try:
+        fname = inputquestion('First name')
+        if fname == 'einde':
+            loop = 0
+        else:
+            lname = inputquestion('Last name')
+            text = inputquestion('tweet')
+            date = definedate()
+    except TypeError:
+        loop = 0
 
-#function for asking the firstname
-def inputfirstnameclient():
-    firstname = input("Firstname : ")
-    return firstname
-#function for asking the lastname
-def inputlastnameclient():
-    lastname = input("Lastname : ")
-    return lastname
-def inputmessage():
-    text = input("Write your text below:\n")
-    return text
-def mainclient():
 
-
-
-    print("{} {}: {}".format(firstname, lastname, text))
-# mainclient()
-
-def inputwritecsv():
+def definedate():
     from datetime import datetime
-    import csv
-    with open('clients.csv', 'w', newline='') as csvfile:
-        # reader = csv.reader(csvfile.csv, delimiter=';')
-        writer = csv.writer(csvfile, delimiter=';')
-        # writer.writerow(('firstname', 'lastname', 'text', 'date'))
-        loop = 1
-        while loop == 1:
-            try:
-                firstname = inputfirstnameclient()
-                if firstname == 'einde':
-                    break
-                lastname = inputlastnameclient()
-                text = inputmessage()
-                date = datetime.time
-                writer.writerow((firstname, lastname, text, date))
-            except TypeError:
-                loop = 0
-inputwritecsv()
+    date = datetime.now().strftime("20%y-%m-%d %H:%M")
+    return date
 
+
+def send():
+    global usercount
+
+
+def inputwritecsv(fname, lname, text, date):
+    import csv
+    loop = 1
+    while loop == 1:
+        with open('clients.csv', 'w', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=';')
+            writer = csv.writer(csvfile, delimiter=';')
+            writer.writerow(('firstname', 'lastname', 'text', 'date'))
+            writer.writerow(fname, lname, text, date)
+        print("Write succesfull!")
+    return loop
+
+
+def clientmenu():
+    clientmenuloop = 0
+    while clientmenuloop == 0:
+        userinput()
+
+        inputwritecsv(fname, lname, text, date)
+
+clientmenu()
+# import datetime
+# date = datetime.datetime.now().strftime("20%y-%m-%d %H:%M")
+# print(date)
