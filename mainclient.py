7aa366169410633ae3493982da35c0bfa17e7d97
tommahs <tommahs@tommahs.nl>
2016-11-01 01:42:07 +0100

    Mainclient ~ 30% done

--- mainclient.py
+++ mainclient.py
@@ -28,28 +28,34 @@ def definedate():
 def send():
     global usercount
 
-
 def inputwritecsv(fname, lname, text, date):
     import csv
-    loop = 1
-    while loop == 1:
-        with open('clients.csv', 'w', newline='') as csvfile:
-            reader = csv.reader(csvfile, delimiter=';')
-            writer = csv.writer(csvfile, delimiter=';')
-            writer.writerow(('firstname', 'lastname', 'text', 'date'))
-            writer.writerow(fname, lname, text, date)
-        print("Write succesfull!")
-    return loop
+    with open('clients.csv', 'w', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=';')
+        writer = csv.writer(csvfile, delimiter=';')
+        writer.writerow(('firstname', 'lastname', 'text', 'date'))
+        line1 = (fname, lname, text, date)
+        writer.writerow(line1)
+    print("Write succesfull!")
+
+def checkdifferences():
+    from general_functions import csv_read, csv_write
+    clientcontent = csv_read('clients.csv')
+    admincontent = csv_read('ReadyForAck.csv')
+    for each in clientcontent:
+        if each not in admincontent:
+            csv_write(ReadyForAck.csv)
+
 
 
 def clientmenu():
+    global clientcounter
+    clientcounter = 0
     clientmenuloop = 0
     while clientmenuloop == 0:
         userinput()
-
         inputwritecsv(fname, lname, text, date)
+        clientcounter +=1
+
 
 clientmenu()
-# import datetime
-# date = datetime.datetime.now().strftime("20%y-%m-%d %H:%M")
-# print(date)
