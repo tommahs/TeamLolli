ffab31720a1536d36631903eccaf084cb69c14d9
Marijn <marijnbecking@gmail.com>
2016-11-01 12:14:35 +0100

    Twitter API tests

--- mainscreen.py
+++ mainscreen.py
@@ -1,11 +1,22 @@
 from authenticator import oauth
 
-r = oauth.request('statuses/user_timeline', {'count': 2,'screen_name' : 'MarijnBecking'})
 
-for item in r.get_iterator():
-    if 'text' in item:
-        message = item['text']
-        date_created_message = item['created_at']
+screenlist = []
+
+# Creating empty list for function
+
+def showmainscreen(amount_of_messages):
+    r = oauth.request('statuses/user_timeline', {'count': amount_of_messages,'screen_name' : 'MarijnBecking'})
+    for item in r.get_iterator():
+        if 'id_str' in item:
+            screenlist.append(item['text'])
+            screenlist.append(item['created_at'])
+            continue
+    return screenlist
+
+# Function to search twitter and form information into the format for the screen inside the station
+# Made into a function for later use
+
 
 
 
