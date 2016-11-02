a8f7fcbbc38090ff65efa04b9dd3c8f07ae5f98d
Marijn <marijnbecking@gmail.com>
2016-11-02 23:05:41 +0100

    Twitter API tests

--- mainscreen.py
+++ mainscreen.py
@@ -9,15 +9,14 @@ def showmainscreen(amount_of_messages):
     r = oauth.request('statuses/user_timeline', {'count': amount_of_messages,'screen_name' : 'MarijnBecking'})
     for item in r.get_iterator():
         if 'id_str' in item:
-            screenlist.append(item['text'])
-            screenlist.append(item['created_at'])
+            s1 = item['text']
+            s2 = item['created_at']
             station = item['entities']['hashtags']
             for d in station:
-                screenlist.append(d['text'])
+                s3 = d['text']
+            screenlist.append([s1,s2,s3])
             continue
     return screenlist
-
-print(showmainscreen(3))
 
 # Function to search twitter and form information into the format for the screen inside the station
 # Made into a function for later use
