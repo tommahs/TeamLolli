e290d764e1866b9753b7aa27b3a8da7d002d46d0
tommahs <tommahs@tommahs.nl>
2016-11-03 19:18:58 +0100

    Usage of Main.py # Client popup works #admin not tested # quit works

--- mainadmin.py
+++ mainadmin.py
@@ -12,10 +12,11 @@ def beoordeel(goedgekeurd, eachtweet, tweetnum):
     import general_functions
     if goedgekeurd is True:
         general_functions.accepted(eachtweet)
-        root.destroy()
     elif goedgekeurd is False:
         general_functions.rejected(eachtweet)
-        root.destroy()
+    elif goedgekeurd == 'exit':
+        exit()
+
 def tweet(lst, tweetnum):
     eachtweet = lst[tweetnum]
     return eachtweet
@@ -31,64 +32,68 @@ def svarmsg(tweet):
 def tweetAck():
     tweetnum = 0
     loop = 0
+    end = 0
     while loop == 0:
+        if end == 1:
+            exit()
         Tweets = updatelist()
         tweetslength = len(Tweets) - 1
         for eachtweet in Tweets:# eachtweet 0 = naam, 1 is bericht, 2 is datum, 3 is stationafkorting
             try:
-                root = Tk()
-                root.title('NS Approval App')
+                maincheck = Tk()
+                maincheck.title('NS Approval App')
 
 
                 ######
                 # Background
                 ####
                 background_image = PhotoImage(file="GUI/NS_logo.png")
-                background_label = Label(root, image=background_image)
+                background_label = Label(maincheck, image=background_image)
                 background_label.grid(rowspan=30, columnspan=12)
-                root.resizable(False, False)
+                maincheck.resizable(False, False)
 
                 ######
                 # Titellabel
                 ####
 
-                label_titel = Label(root, text='NS bericht approval', bg='#FFCC18')
+                label_titel = Label(maincheck, text='NS bericht approval', bg='#FFCC18')
                 label_titel.config(font=('times', 24))
                 label_titel.grid(row=0, columnspan=12, sticky='n')
 
                 ######
                 # Name
                 ####
-                label_2 = Label(root, text='Naam : ', bg='#FFCC18')
+                label_2 = Label(maincheck, text='Naam : ', bg='#FFCC18')
                 label_2.config(font=('times', 24))
                 label_2.grid(row=1, column=2, sticky='ne')
                 ######
                 # Message:
                 ####
-                label_3 = Label(root, text='Bericht : ', bg='#FFCC18')
+                label_3 = Label(maincheck, text='Bericht : ', bg='#FFCC18')
                 label_3.config(font=('times', 24))
                 label_3.grid(row=2, column=2, sticky='ne')
 
 
 
                 ####
-                svar_naam = StringVar(root, value=eachtweet[0])
-                svar_bericht = StringVar(root, value=eachtweet[1])
+                svar_naam = StringVar(maincheck, value=eachtweet[0])
+                svar_bericht = StringVar(maincheck, value=eachtweet[1])
 
-                label_naam = Message(root, textvariable=svar_naam, bg='#e6e6e6', borderwidth=4)
+                label_naam = Message(maincheck, textvariable=svar_naam, bg='#e6e6e6', borderwidth=4)
                 label_naam.config(font=('times', 20), width=400)
                 label_naam.grid(row=1, column=3, columnspan=4, sticky='nw')
 
-                label_message = Message(root, textvariable=svar_bericht, bg='#e6e6e6')
+                label_message = Message(maincheck, textvariable=svar_bericht, bg='#e6e6e6')
                 label_message.grid(row=2, column=3, columnspan=5,  sticky='ew')
                 label_message.config(font=('times', 18), width=400)
 
                 accept = lambda: beoordeel(True, eachtweet, tweetnum)
                 reject = lambda: beoordeel(False, eachtweet, tweetnum)
-                Exit = loop = 1
-                button_reject = Button(root, text='Reject', bg="red", command=reject)
-                button_accept = Button(root, text='accept', bg="Green", command=accept)
-                button_quit = Button(root, text='quit', bg='Gray', command=Exit)
+                quit = lambda: beoordeel("exit", eachtweet, tweetnum)
+
+                button_reject = Button(maincheck, text='Reject', bg="red", command=reject)
+                button_accept = Button(maincheck, text='accept', bg="Green", command=accept)
+                button_quit = Button(maincheck, text='quit', bg='Gray', command=quit)
 
                 button_reject.config(font=('times', 32))
                 button_reject.grid(row=11, column=1)
@@ -100,7 +105,7 @@ def tweetAck():
                 button_quit.config(font=('times', 32))
                 button_quit.grid(row=11, column=5)
                 tweetnum += 1
-                root.mainloop()
+                maincheck.mainloop()
             except:
                 if IndexError:
                     # popup -> wil je doorgaan?
--- mainclient.py
+++ mainclient.py
@@ -38,19 +38,20 @@ def definedate():
     return date
 
 
-def send(newlst):
+def sendtofile(newlst):
     import general_functions
-    # general_functions.csv_writelist('clients', 'clients', newlst, 1)
+    general_functions.csv_writelist('clients', 'clients', newlst, 1)
     print('Clicked on Send!')
 
 # Functie beoordeel zorgt voor een output van True of False, aan de hand daarvan if, else statement maken.
-def beoordeel(goedgekeurd, newlst):
-    if goedgekeurd == True:
-        send(newlst)
-        print('Bedankt!')
-    elif goedgekeurd == False:
-        print('terugnaarhoofdmenu')
-
+# def beoordeel(newlst):
+#         send(newlst)
+#     if goedgekeurd == True:
+#         send(newlst)
+#         print('Bedankt!')
+#     elif goedgekeurd is False:
+#         message = entry_message.get(1.0, END)
+#         return message
 ######
 # Client menu
 # Loops
@@ -62,86 +63,50 @@ def beoordeel(goedgekeurd, newlst):
 ####
 from tkinter import *
 from tkinter.scrolledtext import ScrolledText
-def clientmenu():
+def clientmain(msg):
     import general_functions
-    station = 'Utrecht' #input
-    clientmenuloop = 0
+    station = 'Utrecht'  # input
     station = general_functions.stationabbreviation(station)
-    while clientmenuloop == 0:
-        clientroot = Tk()
-        clientroot.title('NS Reizigers mening')
+    client = Tk()
+    client.wm_title(msg)
 
-        svar_naam = StringVar(clientroot, value='voorbeeld naam')
+    label_titel = Label(client, text='Client', bg='#FFCC18')
+    label_titel.config(font=('times', 24))
+    label_titel.grid(row=0, columnspan=12, sticky='n')
 
-        background_image = PhotoImage(file="GUI/NS_logo.png")
-        background_label = Label(clientroot, image=background_image)
-        background_label.grid(rowspan=30, columnspan=12)
-        clientroot.resizable(False, False)
+    label_2 = Label(client, text='Naam : ', bg='#FFCC18')
+    label_2.config(font=('times', 24))
+    label_2.grid(row=1, column=2, sticky='ne')
 
-        label_titel = Label(clientroot, text='NS bericht approval', bg='#FFCC18')
-        label_titel.config(font=('times', 24))
-        label_titel.grid(row=0, columnspan=12, sticky='n')
+    label_3 = Label(client, text='Bericht : ', bg='#FFCC18')
+    label_3.config(font=('times', 24))
+    label_3.grid(row=2, column=2, sticky='ne')
 
-        label_2 = Label(clientroot, text='Naam : ', bg='#FFCC18')
-        label_2.config(font=('times', 24))
-        label_2.grid(row=1, column=2, sticky='ne')
+    svar_naam = 'testgui'
+    entry_naam = Entry(client, bg='#e6e6e6', borderwidth=4)
+    entry_naam.config(font=('times', 20), width=40)
+    entry_naam.grid(row=1, column=3, columnspan=4, sticky='nw')
+    entry_naam.insert(INSERT, '')
 
-        label_3 = Label(clientroot, text='Bericht : ', bg='#FFCC18')
-        label_3.config(font=('times', 24))
-        label_3.grid(row=2, column=2, sticky='ne')
+    entry_message = ScrolledText(client, width=40, height=4, bg='#e6e6e6')
+    entry_message.config(font=('times', 18))
+    entry_message.grid(row=2, column=3, columnspan=6, sticky='ew')
+    entry_message.insert(INSERT, '')
 
-        entry_naam = Entry(clientroot, textvariable=svar_naam, bg='#e6e6e6', borderwidth=4)
-        entry_naam.config(font=('times', 20), width=40)
-        entry_naam.grid(row=1, column=3, columnspan=4, sticky='nw')
+    # svar_naam = entry_naam.get(1.0, END)
+    svar_bericht = entry_message.get(1.0, END)
+    svar_date = definedate()
+    svar_station = station
+    newlst = [svar_naam, svar_bericht, svar_date, svar_station]
+    print(newlst)
 
-        entry_message = ScrolledText(clientroot, width=40, height=4, bg='#e6e6e6')
-        entry_message.config(font=('times', 18))
-        entry_message.grid(row=2, column=3, columnspan=6, sticky='ew')
-        entry_message.insert(INSERT, '')
+    send = lambda: sendtofile(newlst)
 
-        svar_bericht = entry_message.get(1.0, END)
+    button_cancel = Button(client, text="Cancel", bg='Red', command=client.destroy)
+    button_send = Button(client, text='Verstuur', bg="Green", command=send)
 
-        svar_date = definedate()
-        svar_station = station
-        newlst = [svar_naam, svar_bericht, svar_date, svar_station]
-        print(newlst)
-        cancel = lambda: cancel()
-        send = lambda: beoordeel(True, newlst)
-
-        button_cancel = Button(clientroot, text='Cancel', bg="red", command=cancel())
-        button_send = Button(clientroot, text='Verstuur', bg="Green", command=send)
-
-        button_cancel.config(font=('times', 32))
-        button_cancel.grid(row=11, column=1)
-
-        button_send.config(font=('times', 32))
-        button_send.grid(row=11, column=10)
-        clientroot.mainloop()
-        # if newlst == '1':
-        #     print('Name is too long')
-        #     clientmenuloop = 1
-        # elif newlst == '2':
-        #     print('Text is too long')
-        #     clientmenuloop = 1
-        # else:
-        #     send(newlst)
-        #     general_functions.popuptext('Thank you for giving us valuable feedback to work with!')
-        #     clientmenuloop = 1
-        clientmenuloop = 1
-clientmenu()
-
-
-# # Test to write input into clients.csv
-# from general_functions import *
-#
-# Test to add new tweet to clients.csv
-# mainclient.clientmenu()
-# Test to check to combine clients.csv and ReadyForAck.csv if clientsvalue not yet known voor
-# csv_writelist('ReadyForAck','ReadyForAck', 'clients', 2)
-# input()
-# Test to remove values in logfile from ReadyForAck
-# csv_writelist('ReadyForAck', 'ReadyForAck', 'logfile', 3)
-# input()
-
-
-# Logging
+    button_cancel.config(font=('times', 32))
+    button_cancel.grid(row=10, column=2)
+    button_send.config(font=('times', 32))
+    button_send.grid(row=10, column=7)
+    client.mainloop()
