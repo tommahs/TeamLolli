4e465335763aaa2a6108785427af50f4ee0d8d37
Jasper <jasp3r.kalf@gmail.com>
2016-11-03 17:34:17 +0100

    Showing screen

--- ShowScreenGUI.py
+++ ShowScreenGUI.py
@@ -3,7 +3,7 @@ from mainscreen import showmainscreen
 
 # Starting Root-Window
 RWindow = Tk()
-RWindow.geometry('350x480')
+RWindow.geometry('350x440')
 
 ###################
 
@@ -26,84 +26,90 @@ def toont_weet(naam, bericht,lblnaam):
     svar_labelNaam.set(lblnaam)
     svar_naam.set(naam)
 
-def showtweetsonscreen(amountofmessages):
-    counter = 0
-    newlist = showmainscreen(amountofmessages)
-    lst = []
-    while True:
-        for each in newlist:
-            if counter >= 0 and counter < len(newlist):
-                name = newlist[0][1]
-                message = newlist[counter][0]
-                lst.append(message)
-                counter += 1
+
+counter = 0
+newlist = showmainscreen(3)
+lst = []
+while True:
+    for each in newlist:
+        if counter >= 0 and counter < len(newlist):
+            name = newlist[0][1]
+            message = newlist[counter][0]
+            lst.append(message)
+            counter += 1
+    break
+
+while True:
+    if len(lst) < 10:
+        lst.append(' ')
+    else:
         break
 
-    while True:
-        if len(lst) < 9:
-            lst.append(' ')
-        else:
-            break
 
+# Label Naam
+label_naam = Message(RWindow, textvariable=svar_labelNaam, bg='#e6e6e6', borderwidth=4)
+label_naam.config(font=('times', 16), width=400)
+label_naam.grid(row=1, column=3, columnspan=4, sticky='n', ipadx='100', ipady='10')
+def looping(listofmessages):
+    # Bericht 1
+    label_naam1 = Message(RWindow, textvariable=StringVar(RWindow, value=svar_naam), bg='#e6e6e6')
+    label_naam1.config(font=('times', 12), width=400)
+    label_naam1.grid(row=2, column=3, columnspan=6, sticky='ew', ipadx='100', ipady='10')
 
-    # Label Naam
-    label_naam = Message(RWindow, textvariable=svar_labelNaam, bg='#e6e6e6', borderwidth=4)
-    label_naam.config(font=('times', 16), width=400)
-    label_naam.grid(row=1, column=3, columnspan=4, sticky='n', ipadx='100', ipady='10')
-    def looping(listofmessages):
+    label_message1 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[0]), bg='#e6e6e6')
+    label_message1.config(font=('times', 12), width=400)
+    label_message1.grid(row=3, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
-        # Bericht 1
+    # Bericht 2
+    label_naam2 = Message(RWindow, textvariable=StringVar(RWindow, value=svar_naam), bg='#e6e6e6')
+    label_naam2.config(font=('times', 12), width=400)
+    label_naam2.grid(row=4, column=3, columnspan=6, sticky='ew', ipadx='100', ipady='10')
 
-        label_message1 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[0]), bg='#e6e6e6')
-        label_message1.config(font=('times', 12), width=400)
-        label_message1.grid(row=3, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+    label_message2 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[1]), bg='#e6e6e6')
+    label_message2.config(font=('times', 12), width=400)
+    label_message2.grid(row=5, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
-        # Bericht 2
+    # Bericht 3
+    label_naam3 = Message(RWindow, textvariable=StringVar(RWindow, value=svar_naam), bg='#e6e6e6')
+    label_naam3.config(font=('times', 12), width=400)
+    label_naam3.grid(row=6, column=3, columnspan=6, sticky='ew', ipadx='100', ipady='10')
 
-        label_message2 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[1]), bg='#e6e6e6')
-        label_message2.config(font=('times', 12), width=400)
-        label_message2.grid(row=5, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+    label_message3 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[2]), bg='#e6e6e6')
+    label_message3.config(font=('times', 12), width=400)
+    label_message3.grid(row=7, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
-        # Bericht 3
+    # Bericht 4
+    label_naam4 = Message(RWindow, textvariable=StringVar(RWindow, value=svar_naam), bg='#e6e6e6')
+    label_naam4.config(font=('times', 12), width=400)
+    label_naam4.grid(row=8, column=3, columnspan=6, sticky='ew', ipadx='100', ipady='10')
 
-        label_message3 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[2]), bg='#e6e6e6')
-        label_message3.config(font=('times', 12), width=400)
-        label_message3.grid(row=7, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+    label_message4 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[3]), bg='#e6e6e6')
+    label_message4.config(font=('times', 12), width=400)
+    label_message4.grid(row=9, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
-        # Bericht 4
-
-        label_message4 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[3]), bg='#e6e6e6')
-        label_message4.config(font=('times', 12), width=400)
-        label_message4.grid(row=9, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
-
-        # Bericht 5
-        label_message5 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[4]), bg='#e6e6e6')
-        label_message5.config(font=('times', 12), width=400)
-        label_message5.grid(row=6, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
-
-        # Bericht 6
-        label_message6 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[5]), bg='#e6e6e6')
-        label_message6.config(font=('times', 12), width=400)
-        label_message6.grid(row=7, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
-
-        # Bericht 7
-        label_message7 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[6]), bg='#e6e6e6')
-        label_message7.config(font=('times', 12), width=400)
-        label_message7.grid(row=8, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
-
-        # Bericht 8
-        label_message8 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[7]), bg='#e6e6e6')
-        label_message8.config(font=('times', 12), width=400)
-        label_message8.grid(row=9, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
-
-        # Bericht 9
-        label_message9 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[8]), bg='#e6e6e6')
-        label_message9.config(font=('times', 12), width=400)
-        label_message9.grid(row=10, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
-
-    looping(lst)
-    mainloop()
-
-
-# def startlooping():
-showtweetsonscreen(5)
+    # # Bericht 5
+    # label_message5 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[4]), bg='#e6e6e6')
+    # label_message5.config(font=('times', 12), width=400)
+    # label_message5.grid(row=6, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+    #
+    # # Bericht 6
+    # label_message6 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[5]), bg='#e6e6e6')
+    # label_message6.config(font=('times', 12), width=400)
+    # label_message6.grid(row=7, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+    #
+    # # Bericht 7
+    # label_message7 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[6]), bg='#e6e6e6')
+    # label_message7.config(font=('times', 12), width=400)
+    # label_message7.grid(row=8, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+    #
+    # # Bericht 8
+    # label_message8 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[7]), bg='#e6e6e6')
+    # label_message8.config(font=('times', 12), width=400)
+    # label_message8.grid(row=9, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+    #
+    # # Bericht 9
+    # label_message9 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[9]), bg='#e6e6e6')
+    # label_message9.config(font=('times', 12), width=400)
+    # label_message9.grid(row=10, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+looping(lst)
+mainloop()
