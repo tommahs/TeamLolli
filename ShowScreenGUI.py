bd9a8de63399b50aa4b4452a0e8ce1784a82f4ea
Marijn <marijnbecking@gmail.com>
2016-11-04 00:41:22 +0100

    Twitter API tests

--- ShowScreenGUI.py
+++ ShowScreenGUI.py
@@ -1,5 +1,10 @@
 from tkinter import *
 from mainscreen import showmainscreen
+import tkinter
+import sched, time
+
+
+
 
 # Starting Root-Window
 RWindow = Tk()
@@ -26,14 +31,14 @@ def toont_weet(naam, bericht,lblnaam):
     svar_labelNaam.set(lblnaam)
     svar_naam.set(naam)
 
-def showmainscreenloop():
+def createlist():
     counter = 0
-    newlist = showmainscreen(8)
     lst = []
+    newlist = showmainscreen(9)
     while True:
         for each in newlist:
             if counter >= 0 and counter < len(newlist):
-                name = newlist[0][1]
+
                 message = newlist[counter][0]
                 lst.append(message)
                 counter += 1
@@ -44,62 +49,77 @@ def showmainscreenloop():
             lst.append(' ')
         else:
             break
+    return lst
 
 
     # Label Naam
-    label_naam = Message(RWindow, textvariable=svar_labelNaam, bg='#e6e6e6', borderwidth=5)
-    label_naam.config(font=('times', 16), width=400)
-    label_naam.grid(row=1, column=4, columnspan=4, sticky='n', ipadx='100', ipady='10')
-    def looping(listofmessages):
-        # Bericht 1
-        # label_naam1 = Message(RWindow, textvariable=StringVar(RWindow, value=svar_naam), bg='#e6e6e6')
-        # label_naam1.config(font=('times', 12), width=400)
-        # label_naam1.grid(row=2, column=3, columnspan=6, sticky='ew', ipadx='100', ipady='10')
 
-        label_message1 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[0]), bg='#e6e6e6')
-        label_message1.config(font=('times', 12), width=400)
-        label_message1.grid(row=2, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_naam = Message(RWindow, textvariable=svar_labelNaam, bg='#e6e6e6', borderwidth=5)
+label_naam.config(font=('times', 16), width=400)
+label_naam.grid(row=1, column=4, columnspan=4, sticky='n', ipadx='100', ipady='10')
+
+listofmessages = createlist()
+
+        # Bericht 1
+
+label_message1 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[0]), bg='#e6e6e6')
+label_message1.config(font=('times', 12), width=400)
+label_message1.grid(row=2, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # Bericht 2
-        label_message2 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[1]), bg='#b7b7b7')
-        label_message2.config(font=('times', 12), width=400)
-        label_message2.grid(row=3, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_message2 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[1]), bg='#b7b7b7')
+label_message2.config(font=('times', 12), width=400)
+label_message2.grid(row=3, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # Bericht 3
-        label_message3 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[2]), bg='#e6e6e6')
-        label_message3.config(font=('times', 12), width=400)
-        label_message3.grid(row=4, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_message3 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[2]), bg='#e6e6e6')
+label_message3.config(font=('times', 12), width=400)
+label_message3.grid(row=4, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # Bericht 4
-        label_message4 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[3]), bg='#b7b7b7')
-        label_message4.config(font=('times', 12), width=400)
-        label_message4.grid(row=5, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_message4 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[3]), bg='#b7b7b7')
+label_message4.config(font=('times', 12), width=400)
+label_message4.grid(row=5, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # # Bericht 5
-        label_message5 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[4]), bg='#e6e6e6')
-        label_message5.config(font=('times', 12), width=400)
-        label_message5.grid(row=6, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_message5 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[4]), bg='#e6e6e6')
+label_message5.config(font=('times', 12), width=400)
+label_message5.grid(row=6, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # Bericht 6
-        label_message6 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[5]), bg='#b7b7b7')
-        label_message6.config(font=('times', 12), width=400)
-        label_message6.grid(row=7, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_message6 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[5]), bg='#b7b7b7')
+label_message6.config(font=('times', 12), width=400)
+label_message6.grid(row=7, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # Bericht 7
-        label_message7 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[6]), bg='#e6e6e6')
-        label_message7.config(font=('times', 12), width=400)
-        label_message7.grid(row=8, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_message7 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[6]), bg='#e6e6e6')
+label_message7.config(font=('times', 12), width=400)
+label_message7.grid(row=8, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # Bericht 8
-        label_message8 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[7]), bg='#b7b7b7')
-        label_message8.config(font=('times', 12), width=400)
-        label_message8.grid(row=9, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
+label_message8 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[7]), bg='#b7b7b7')
+label_message8.config(font=('times', 12), width=400)
+label_message8.grid(row=9, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
         # Bericht 9
-        label_message9 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[9]), bg='#e6e6e6')
-        label_message9.config(font=('times', 12), width=400)
-        label_message9.grid(row=10, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
-    looping(lst)
-    mainloop()
+label_message9 = Message(RWindow, textvariable = StringVar(RWindow, value=listofmessages[8]), bg='#e6e6e6')
+label_message9.config(font=('times', 12), width=400)
+label_message9.grid(row=10, column=3, columnspan=6,  sticky='ew', ipadx='100', ipady='10')
 
-showmainscreenloop()
+
+def loop():
+    listofmessages1 = createlist()
+    label_message1.config(textvariable=StringVar(RWindow, value=listofmessages1[0]))
+    label_message2.config(textvariable=StringVar(RWindow, value=listofmessages1[1]))
+    label_message3.config(textvariable=StringVar(RWindow, value=listofmessages1[2]))
+    label_message4.config(textvariable=StringVar(RWindow, value=listofmessages1[3]))
+    label_message5.config(textvariable=StringVar(RWindow, value=listofmessages1[4]))
+    label_message6.config(textvariable=StringVar(RWindow, value=listofmessages1[5]))
+    label_message7.config(textvariable=StringVar(RWindow, value=listofmessages1[6]))
+    label_message8.config(textvariable=StringVar(RWindow, value=listofmessages1[7]))
+    label_message9.config(textvariable=StringVar(RWindow, value=listofmessages1[8]))
+    RWindow.after(5000,loop)
+
+
+RWindow.after(8000, loop)
+RWindow.mainloop()
--- mainscreen.py
+++ mainscreen.py
@@ -6,19 +6,21 @@ screenlist = []
 # Creating empty list for function
 
 def showmainscreen(amount_of_messages):
-    r = oauth.request('statuses/user_timeline', {'count': amount_of_messages,'screen_name' : 'MarijnBecking'})
-    for item in r.get_iterator():
-        if 'id_str' in item:
-            s1 = item['text']
-            s2 = item['created_at']
-            station = item['entities']['hashtags']
-            for d in station:
-                s3 = d['text']
-            screenlist.append([s1,s2,s3])
-            continue
-    return screenlist
+    screenlist.clear()
+    """"Using showmainscreen creates a list used for showscreenGUI"""
+    while True:
+        r = oauth.request('statuses/user_timeline', {'count': amount_of_messages,'screen_name' : 'MarijnBecking'})
+        for item in r.get_iterator():
+            if 'id_str' in item:
+                s1 = item['text']
+                s2 = item['created_at']
+                station = item['entities']['hashtags']
+                for d in station:
+                    s3 = d['text']
+                screenlist.append([s1,s2,s3])
+                continue
+        return screenlist
 
-print(showmainscreen(3))
 # Function to search twitter and form information into the format for the screen inside the station
 # Made into a function for later use
 
